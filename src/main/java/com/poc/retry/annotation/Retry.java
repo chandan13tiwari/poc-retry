895fc511b79d714521e04a4bd27f6d26311817e8
Chandan Tiwari <chandan13tiwaris@gmail.com>
2024-01-27 10:56:02 +0530

    added java docs

--- src/main/java/com/poc/retry/annotation/Retry.java
+++ src/main/java/com/poc/retry/annotation/Retry.java
@@ -7,10 +7,51 @@ import java.time.temporal.ChronoUnit;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface Retry {
+  /**
+   * Maximum number of retries we can trigger for that particular method. It should be a positive integer.
+   * Zero(0) or a Negative integer can be provided as maximum number of retries.
+   * By default, Maximum number of retries is set to 10, which means in case of any failure, it will trigger
+   * the method 10 times to the max.
+   */
   int times() default 10;
+
+  /**
+   * Time interval between two retries.
+   * Time interval could be in any TimeUnit, you just need to set the time as a long value.
+   * You need to set the retryInterval TimeUnit by setting {@link Retry#retryIntervalTimeUnit}
+   * By default, retryInterval is set to 5 seconds, which means between first retry there is 5 seconds
+   * of wait.
+   */
   long retryInterval() default 5L;
+
+  /**
+   * Initial delay for the first retry.
+   * Initial delay could be in any TimeUnit, you just need to set the time as a Long value.
+   * You need to set the initialDelay TimeUnit by setting {@link Retry#initialDelayTimeUnit}
+   * By default, initialDelay is set to 0 seconds, which means there is no initial delay by default.
+   */
   long initialDelay() default 0L;
+
+  /**
+   * Multiplier is the algorithm to trigger the next retry exponentially.
+   * It should be an integer value. Zero or negative values are not supported.
+   * By default, multiplier value is set to 2, which means the time interval between retries are
+   * increased exponentially.
+   *
+   * @see "Ex., retryInterval is 5, then
+   * first retry will trigger after 5x2=10seconds
+   * second retry will trigger after 10x2=20seconds
+   * third retry will trigger after 20x2=40seconds and so on.."
+   */
   int multiplier() default 2;
+
+  /**
+   * TimeUnit for Retry Interval
+   */
   ChronoUnit retryIntervalTimeUnit() default ChronoUnit.SECONDS;
+
+  /**
+   * TimeUnit for Initial Delay
+   */
   ChronoUnit initialDelayTimeUnit() default ChronoUnit.SECONDS;
 }
--- src/test/java/com/poc/retry/RetryApplication.java
+++
@@ -1,7 +0,0 @@
-package com.poc.retry;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class RetryApplication {
-}
